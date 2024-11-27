aac94baab6f0170754c1b8007c70597df58b9b45
rajanmishra <rajanmishra01v@gmail.com>
2024-11-27 11:05:05 +0530

    test 1

---
+++ src/main/java/com/crm/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.crm.controller;
+
+import com.crm.entity.Post;
+import com.crm.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public String createPost(@RequestBody Post post){
+        postService.addPost(post);
+        return "Post added";
+    }
+    @DeleteMapping
+    public void deletePost(){
+        postService.deletePost();
+
+    }
+}
